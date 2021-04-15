5655a736e42b33dee8cdab9fcc666eec6853bd47
Sachindra Singh <sachindras@spc.int>
2021-04-15 16:14:02 +1200

    Alert indicator added to inundation page

--- inundation.js
+++ inundation.js
@@ -56,16 +56,16 @@ function onMapClick(e) {
         .setContent(e.containerPoint.x + ", " + e.containerPoint.y + " : " + e.latlng)
         .openOn(map);
 
-        //var isrc = "http://192.168.8.100:8080/ncWMS2/wms?REQUEST=GetTimeseries&LAYERS=1/Hswell&QUERY_LAYERS=1/Hswell,1/Tp,1/L,1/Hs&BBOX=176.7314,-19.385,178.8734,-17.6714&SRS=CRS:84&FEATURE_COUNT=5&HEIGHT=500&WIDTH=950&X=" + Math.round(e.containerPoint.x) + "&Y="+ Math.round(e.containerPoint.y) + "&STYLES=default/default&VERSION=1.1.1&TIME=2021-02-18T00:00:00.000Z/2021-02-25T00:00:00.000Z&INFO_FORMAT=image/png&CHARTWIDTH=790"
-        //document.getElementById("static-chart").src = isrc;
-    
-        //http://192.168.8.100:8080/ncWMS2/wms?REQUEST=GetTimeseries&LAYERS=1/Hswell&QUERY_LAYERS=1/Hswell,1/Tp,1/L,1/Hs&BBOX=176.7314,-19.385,178.8734,-17.6714&SRS=CRS:84&FEATURE_COUNT=5&HEIGHT=500&WIDTH=950&X=434&Y=361&STYLES=default/default&VERSION=1.1.1&TIME=2021-02-18T00:00:00.000Z/2021-02-25T00:00:00.000Z&INFO_FORMAT=image/png&CHARTWIDTH=790
-        //http://localhost:8080/ncWMS2/wms?REQUEST=GetTimeseries&LAYERS=1/Hswell&QUERY_LAYERS=1/Hs,1/Hswell,1/Tp,1/L&BBOX=176.7314,-19.385,178.8734,-17.6714&SRS=CRS:84&FEATURE_COUNT=5&HEIGHT=500&WIDTH=950&X=434&Y=361&STYLES=default/default&VERSION=1.1.1&TIME=2021-02-18T00:00:00.000Z/2021-02-25T00:00:00.000Z&INFO_FORMAT=image/png
-   
+    //var isrc = "http://192.168.8.100:8080/ncWMS2/wms?REQUEST=GetTimeseries&LAYERS=1/Hswell&QUERY_LAYERS=1/Hswell,1/Tp,1/L,1/Hs&BBOX=176.7314,-19.385,178.8734,-17.6714&SRS=CRS:84&FEATURE_COUNT=5&HEIGHT=500&WIDTH=950&X=" + Math.round(e.containerPoint.x) + "&Y="+ Math.round(e.containerPoint.y) + "&STYLES=default/default&VERSION=1.1.1&TIME=2021-02-18T00:00:00.000Z/2021-02-25T00:00:00.000Z&INFO_FORMAT=image/png&CHARTWIDTH=790"
+    //document.getElementById("static-chart").src = isrc;
+
+    //http://192.168.8.100:8080/ncWMS2/wms?REQUEST=GetTimeseries&LAYERS=1/Hswell&QUERY_LAYERS=1/Hswell,1/Tp,1/L,1/Hs&BBOX=176.7314,-19.385,178.8734,-17.6714&SRS=CRS:84&FEATURE_COUNT=5&HEIGHT=500&WIDTH=950&X=434&Y=361&STYLES=default/default&VERSION=1.1.1&TIME=2021-02-18T00:00:00.000Z/2021-02-25T00:00:00.000Z&INFO_FORMAT=image/png&CHARTWIDTH=790
+    //http://localhost:8080/ncWMS2/wms?REQUEST=GetTimeseries&LAYERS=1/Hswell&QUERY_LAYERS=1/Hs,1/Hswell,1/Tp,1/L&BBOX=176.7314,-19.385,178.8734,-17.6714&SRS=CRS:84&FEATURE_COUNT=5&HEIGHT=500&WIDTH=950&X=434&Y=361&STYLES=default/default&VERSION=1.1.1&TIME=2021-02-18T00:00:00.000Z/2021-02-25T00:00:00.000Z&INFO_FORMAT=image/png
+
 }
 map.on('click', onMapClick);
 
-var overlayMaps = { 
+var overlayMaps = {
     "Cuvu Inundation Site 1": tdLayer_In,
 };
 
@@ -73,26 +73,22 @@ var baseLayers = getCommonBaseLayers(map);
 var layerControl = L.control.layers(baseLayers, overlayMaps).addTo(map);
 
 
-function populateLayers()
-{
+function populateLayers() {
     var wmsUrl = host + "ncWMS2/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", "data.jsp", false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 var text = rawFile.responseText.trim();
                 arr = text.split("\n");
-                
-                for (i = 0; i < arr.length; i++){
+
+                for (i = 0; i < arr.length; i++) {
                     layerName = arr[i];
-                    if (!layerName.includes(".png")){
+                    if (!layerName.includes(".png")) {
                         console.log(layerName);
                         //add layer to map
-                        
+
                         var layer = L.tileLayer.wms(wmsUrl, {
                             layers: layerName + "/z",
                             format: 'image/png',
@@ -103,12 +99,12 @@ function populateLayers()
                             numcolorbands: 250,
                             styles: 'default-scalar/x-Sst',
                             //styles: 'default-scalar/div-BuRd',
-                            attribution: 'SPC CIFDP',   
+                            attribution: 'SPC CIFDP',
                             //times: ['2021-04-10T06'],                            
                             //times: '2021-04-07T11:00:00.000Z'
                         });
 
-                       
+
                         //var td = L.TimeDimension({
                         //    times: '2020-12-07T11:00:00.000Z',
                         //});
@@ -126,7 +122,7 @@ function populateLayers()
                             //}
                             //times: '2021-04-07T11:00:00.000Z',
                             //timeDimension.times: '2021-04-07T11:00:00.000Z',
-                            
+
                         });
 
                         //console.log(td_Layer.getCurrentTime());
@@ -136,9 +132,9 @@ function populateLayers()
                         //layerControl.addOverlay(layerName, layer);
 
                         //td_Layer.addTo(map);
-                        
 
-                        
+
+
 
                     }
                 }
@@ -148,5 +144,55 @@ function populateLayers()
     rawFile.send(null);
 }
 
+var redIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+var greenIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-green.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+
+var icontest = new L.Icon({
+    iconUrl: 'img/alert.gif',
+    iconSize: [41, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+
+var myFeatureGroup = L.featureGroup().addTo(map);
+var marker, test;
+
+var locations = [
+    ["Cuvu", -18.134225, 177.418035, "site1"],
+    ["Cuvu", -18.145161, 177.422334, "site2"],
+    ["Cuvu", -18.168178, 177.466828, "site3"],
+    ["Korotogo", -18.170216, 177.534531, "site1"],
+    ["Korotogo", -18.181435, 177.562147, "site2"],
+    ["Korotogo", -18.184183, 177.609472, "site3"],
+    ["Komave", -18.227696, 177.772865, "site1"],
+    ["Komave", -18.221285, 177.758369, "site2"],
+    ["Komave", -18.213895, 177.729305, "site3"],
+    ["MauiBay", -18.204506, 177.673125, "site1"]
+];
+for (var i = 0; i < locations.length; i++) {
+    var vv;
+    if (locations[i][0] == "Cuvu") {
+        vv = icontest;
+    }
+    else {
+        vv = icontest;
+    }
+    marker = new L.marker([locations[i][1], locations[i][2]], { icon: vv }).addTo(myFeatureGroup).bindPopup("<center><h3>" + locations[i][0] + "-" + locations[i][3] + "</h3></center><img style='width: 740px;' src='img/" + locations[i][0] + "-WL-" + locations[i][3] + ".png'>", { maxWidth: "auto" }).bindTooltip(locations[i][0] + "-" + locations[i][3]);
+}
+
+
 populateLayers();
+
+
+
 
